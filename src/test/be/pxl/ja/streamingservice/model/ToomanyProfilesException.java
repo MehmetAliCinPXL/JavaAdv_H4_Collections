06889cfb759c277a7bc8bbeaf0ea77055423f161
MehmetAliCinPXL <MehmetAli.Cin@student.pxl.be>
2020-11-19 00:04:38 +0100

    Exception test toegevoegd

---
+++ src/test/be/pxl/ja/streamingservice/model/ToomanyProfilesException.java
@@ -0,0 +1,21 @@
+package test.be.pxl.ja.streamingservice.model;
+
+import be.pxl.ja.streamingservice.exception.InvalidDateException;
+import be.pxl.ja.streamingservice.exception.TooManyProfilesException;
+import be.pxl.ja.streamingservice.model.Account;
+import be.pxl.ja.streamingservice.model.Profile;
+import be.pxl.ja.streamingservice.model.StreamingPlan;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ToomanyProfilesException {
+    @Test
+    public void throwsInvalidDateExceptionWhenMaximumProfilesReached() {
+        Account acc = new Account("JohnDoe@example.be", "1234");
+        acc.setStreamingPlan(StreamingPlan.BASIC);
+        assertThrows(TooManyProfilesException.class, () -> acc.addProfile(new Profile("John Doe")));
+
+    }
+}
